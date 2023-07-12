b2f327a5fe1f97eb7d3fa5336db9926220151c59
Khalid Jameer <kjameer0@gmail.com>
2023-07-12 16:37:23 -0400

    chore: added homework1 file

---
+++ teach/homework1.js
@@ -0,0 +1,7 @@
+//make sure you push when you're done! and pull first
+
+// https://the-winter.github.io/codingjs/exercise.html?name=helloName&title=String-1
+
+//https://the-winter.github.io/codingjs/exercise.html?name=makeAbba&title=String-1
+
+//https://the-winter.github.io/codingjs/exercise.html?name=makeTags&title=String-1
