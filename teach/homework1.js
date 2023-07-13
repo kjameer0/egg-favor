d93028a8261c5813ca62a066f7e3389f52ac7899
de289 <127776911+de289@users.noreply.github.com>
2023-07-13 11:52:17 -0400

    solved String-1 -- helloName

--- teach/homework1.js
+++ teach/homework1.js
@@ -1,7 +1,24 @@
 //make sure you push when you're done! and pull first
 
 // https://the-winter.github.io/codingjs/exercise.html?name=helloName&title=String-1
+/*
+String-1 -- helloName
+Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+Given name, return 'Hello name!'
+Concatenate 'Hello' + ' ' + name + ! 
+*/
+
+function helloName(name) {
+    return 'Hello' + ' ' + name + '!';
+}
+
+
 
 //https://the-winter.github.io/codingjs/exercise.html?name=makeAbba&title=String-1
 
+
+
 //https://the-winter.github.io/codingjs/exercise.html?name=makeTags&title=String-1
+
+
